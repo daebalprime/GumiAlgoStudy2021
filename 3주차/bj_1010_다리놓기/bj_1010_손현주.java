380067570abc02cefd1dc77b304cc7f9d58302c4
HyeonJuSon <kkobukk95@gmail.com>
2021-02-17 10:21:04 +0900

    3주차 손현주

--- 3주차/bj_1010_다리놓기/bj_1010_손현주.java
+++ 3주차/bj_1010_다리놓기/bj_1010_손현주.java
@@ -5,10 +5,10 @@ public class bj_1010 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
-		int T = sc.nextInt();
-		for(int tc=1; tc<=T; tc++) {
-			int R = sc.nextInt();
-			int N = sc.nextInt();
+		int T = sc.nextInt();							// 테케 입력
+		for(int tc=1; tc<=T; tc++) { 
+			int R = sc.nextInt(); 						// 서쪽 N => nPr 편의를 위해 변수명 수정  
+			int N = sc.nextInt(); 						// 동쪽 M
 			sb.append(Combination(N,R)).append('\n');
 		}
 		System.out.println(sb);
@@ -16,8 +16,8 @@ public class bj_1010 {
 	}
 	
 	public static int Combination(int n, int r) {
-		if(isVisited[n][r]>0) return isVisited[n][r];
-		if(n==r || r ==0) return isVisited[n][r] = 1;
+		if(isVisited[n][r]>0) return isVisited[n][r]; 	// 이미 방문한(저장되어있는)경우 리턴
+		if(n==r || r ==0) return isVisited[n][r] = 1; 	// nPn, nP1 경우 1이니까 리턴
 		else return isVisited[n][r] = Combination(n-1, r-1)+Combination(n-1,r);
 	}
 
